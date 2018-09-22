848ff5311d2a7a024c336130fda7d8499748bbe3
Mayara <Mayara@192.168.3.4>
2018-09-22 19:30:58 -0300

    Criando projeto Criado
    Signed-off-by: Mayara <Mayara@192.168.3.4>

---
+++ TrabalhoAv1/src/modelo/Aluno.java
@@ -0,0 +1,65 @@
+package modelo;
+
+public class Aluno {
+	private String nome;
+	private long cpf;
+	private long matricula;
+	private String email;
+	private long telefone;
+	private boolean situacao;
+
+	public Aluno() {
+		
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public long getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(long cpf) {
+		this.cpf = cpf;
+	}
+
+	public long getMatricula() {
+		return matricula;
+	}
+
+	public void setMatricula(long matricula) {
+		this.matricula = matricula;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public long getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(long telefone) {
+		this.telefone = telefone;
+	}
+
+	public boolean isSituacao() {
+		return situacao;
+	}
+
+	public void setSituacao(boolean situacao) {
+		this.situacao = situacao;
+	}
+	
+	
+
+}
